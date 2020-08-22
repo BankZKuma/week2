84fddab91e4cfd46b5ba10562ae472bb8db2afd6
BankZKuma <69130573+BankZKuma@users.noreply.github.com>
2020-08-23 01:10:45 +0700

    Update Source1.cpp

--- week2/Source1.cpp
+++ week2/Source1.cpp
@@ -4,25 +4,32 @@
 #include<stdlib.h>
 #include<math.h>
 int main() {
-	int number[100];
-	int sum = 0, count = 0;
-	srand(time(NULL));
-	for (int i = 0; i < 100; i++) {
-		number[i] = rand() % 1001;
-	}
-	int max = number[0];
-	int min = number[0];
-	for (int i = 0; i < 100; i++) {
-		number[i] = number[i] < min ? min : max;
-	}
-	for (int i = 1; i <= max; i++) {
-		if (max % i == 0) {
-			count++;
+	int num;
+	printf("Enter 1 play\n");
+	printf("Enter 0 show one number  and exit\n");
+	scanf_s("%d", &num);
+	do {
+		int number[100];
+		int sum = 0, count = 0;
+		srand(time(NULL));
+		for (int i = 0; i < 100; i++) {
+			number[i] = rand() % 1001;
 		}
-	}
-	if (count == 2) {
-		printf("%d is Prime", max);
-	}
-	else if (count != 2)
-		printf("%d is NotPrime", max);
+		int max = number[0];
+		int min = number[0];
+		for (int i = 0; i < 100; i++) {
+			number[i] = number[i] < min ? min : max;
+		}
+		for (int i = 1; i <= max; i++) {
+			if (max % i == 0) {
+				count++;
+			}
+		}
+		if (count == 2) {
+			printf("%d is Prime\n", max);
+		}
+		else if (count != 2)
+			printf("%d is NotPrime\n", max);
+	} while (num != 0);
+		return 0;
 }
